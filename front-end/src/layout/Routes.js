55f767ff5c0b435228b95e50534b7ada72d4ef2d
David Hindman <david.w.hindman@gmail.com>
2023-06-30 09:51:50 -0400

    Updated packages to have original starter code and installed npm.

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import useQuery from "../utils/useQuery";
 import CreateReservation from "../Reservations/CreateReservation";
 
 /**
@@ -14,6 +15,9 @@ import CreateReservation from "../Reservations/CreateReservation";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const query = useQuery();
+  const date = query.get("date");
+
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -23,7 +27,7 @@ function Routes() {
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={date ? date : today()} />
       </Route>
       <Route path="/reservations/new">
         <CreateReservation />
